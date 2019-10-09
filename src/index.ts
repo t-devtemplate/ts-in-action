d01423521b6148e7327bbcabe08514291dec52ee
luozhijun <lzj586586@163.com>
2019-10-09 22:41:06 +0800

    类: 继承和成员修饰

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import './enum'
 import './object_type_interface'
 import './function_type_interface'
 import './function'
+import './class'
 
 let hello: string = 'hello typescript';
 document.querySelectorAll('.app')[0].innerHTML = hello;
