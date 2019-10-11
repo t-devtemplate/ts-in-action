ccba0314a11b69f6feb554e8a06b4b0f6005f3f0
luozhijun <lzj586586@163.com>
2019-10-11 21:31:04 +0800

    类: 抽象类与多态

--- src/class.ts
+++ src/class.ts
@@ -1,38 +1,62 @@
 
-class Dog{
-  constructor(name: string){
-    // this.name = name;
-  }
-  name?: string;
-  run(){}
-  private pri(){
+abstract class Animal{
 
+  // 定义一个有具体的实现方法, 这样子类就不用实现了, 实现了方法的复用
+  eat() {
+    console.log('eat');
   }
-  protected pro(){
-
-  }
-  readonly legs: number = 4;
-  static food:string = 'bones';
+  // 没有具体实现的抽象方法, 抽象方法的好处就是你明知道子类有其他的实现, 就没必要在父类实现了
+  abstract sleep():void
 }
 
-console.log(Dog.prototype);
+// let animal = new Animal()
+
+class Dog extends Animal{
+  constructor(name: string){
+    super();
+    this.name = name;
+  }
+  name: string;
+  run(){}
+  sleep(){
+    console.log('dog sleep');
+  }
+
+}
 
 let dog = new Dog('wangwang');
-console.log(dog);
-// dog.pri()
-// dog.pro()
-console.log(Dog.food);
+dog.eat();
 
-class Husky extends Dog{
-  constructor(name: string, public color: string){
-    super(name);
-    // this.color = color;
-    this.pro();
+
+class Cat extends Animal{
+  sleep(){
+    console.log("Cat sleep");
   }
 }
 
-console.log(Husky.food);
+let cat = new Cat();
 
+let animals: Animal[] = [dog, cat];
+animals.forEach((value) => {
+  value.sleep(); // 实现多态
+})
 
+// 类的成员方法可以直接返回this, 实现链式调用
+class WorkFlow{
+  step1(){
+    return this;
+  }
+  step2(){
+    return this;
+  }
+}
+new WorkFlow().step1().step2();
+
+class MyFlow extends WorkFlow{
+  next(){
+    return this;
+  }
+}
+new MyFlow().next().step1().next().step2();
 
 export {};
