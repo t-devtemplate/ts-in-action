6d313fcfd56e79f68a85ade19b9d5b8405c8b6ee
luozhijun <lzj586586@163.com>
2019-12-19 11:47:51 +0800

    ts-jest单元测试

---
+++ test/math.test.ts
@@ -0,0 +1,14 @@
+const math = require('../src/math')
+
+
+test('add: 1 + 1 = 2', () => {
+  expect(math.add(1, 1)).toBe(2);
+})
+
+test('sub: 1 - 2', () => {
+  expect(math.sub(1, 2)).toBe(-1);
+})
+
+
+let x:number = '1';
+
